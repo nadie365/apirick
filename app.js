4a0f81568a15e3d354f497b86a0bd4834607f920
douglas <douglashernandez621@gmail.com>
2023-05-27 18:58:03 -0600

    api sin paginacion

---
+++ app.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetchData();
+})
+
+const fetchData = async () => {
+    // console.log("obteniendo datos ...");
+    try {
+        loadingData(true);
+
+        const res = await fetch("https://rickandmortyapi.com/api/character");
+        const data = await res.json();
+
+        // console.log(data.results)
+        pintarCard(data);
+    } catch (error) {
+        console.log(error)
+    }finally{
+        loadingData(false)
+
+    }
+}
+const pintarCard = (data) => {
+    const cards = document.getElementById('card-dinamicas');
+    const templateCard = document.getElementById('template-card').content;
+    const fragment = document.createDocumentFragment();
+    // console.log(data);
+
+    data.results.forEach((item) => {
+        // console.log(item)
+        const clone = templateCard.cloneNode(true)
+        clone.querySelector("h5").textContent = item.name;
+        clone.querySelector("p").textContent = item.species;
+        clone.querySelector(".card-img-top").setAttribute("src",item.image)
+        
+        // Guardamos en el fragment para evitar el reflow
+        fragment.appendChild(clone);
+    });
+    cards.appendChild(fragment);
+};
+
+const loadingData =  (estado) => {
+    const loading =  document.getElementById("loading")
+    if(estado){
+        loading.classList.remove('d-none')
+    }else{
+        loading.classList.add('d-none')
+
+    }
+}
